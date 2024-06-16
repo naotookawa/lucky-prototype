b0f99385dc7039651fd052e65afa58458ad7f53b
naotookawa <na.okawa04@gmail.com>
2024-06-16 16:03:45 +0000

    edited

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 @app.route('/test')
-def hello_world():
+def print_lucky():
     return 'lucky'
 
 # おまじない
